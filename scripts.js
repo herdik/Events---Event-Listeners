6ed6667b7d0fc56f8c0153862ef2c17cefa0b1ba
herdik <juraj.herda1983@gmail.com>
2022-01-25 18:53:16 +0100

    Exercise 1,2,3

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+
+
+  $('#btn1').on('click', () => { 
+    console.log('Yeah, you clicked me'); 
+  });
+
+ 
+
+
+
+$('#btn2').on('click', () => { 
+    $('#btn1').text("Remove"); 
+ 
+  });
+
+  
+
+
+$('#btn3').click(() => {
+    $('button').css('background', 'green');
+    
+  })
+
+  
