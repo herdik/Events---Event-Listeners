3a4482c76adcb7782c706f4a43f74755a32b15e2
herdik <juraj.herda1983@gmail.com>
2022-01-26 08:14:06 +0100

    Exercise 2

--- scripts.js
+++ scripts.js
@@ -1,33 +1,35 @@
 
 
 
-  $('#btn1').on('click', () => { 
+  $('#btn1').on('click', function(e){
     console.log('Yeah, you clicked me'); 
+    $( this ).off(e); /* Exercise 2 */
   });
 
  
 
 
 
-$('#btn2').on('click', () => { 
+$('#btn2').on('click', (e) => { 
     $('#btn1').text("Remove"); 
- 
+    $( this ).off(e); /* Exercise 2 */
   });
 
   
 
 
-$('#btn3').click(() => {
+$('#btn3').click((e) => {
     $('button').css('background', 'green');
-    
+    $( this ).off(e); /* Exercise 2 */
   })
 
 
   /* Add some fun feature to your Event Listeners */
 
   /* Exercise 1 */
-  
-  $('#btn3').click(() => {
-	let color = $('input').val();
-	$('button').css('background-color', `${color}`);
-});
+  $("#btn3").click(function(e) {
+    let color = $('input').val();
+	$('button').css('background-color', `${color}`)
+  $( this ).off(e); /* Exercise 2 */
+  });
+
